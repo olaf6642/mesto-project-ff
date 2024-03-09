33e32da8eb4a8238b7023e96f9da8ad2e7c38afe
Anton Rubtsov <ant.rbox@yandex.ru>
2024-03-09 21:20:28 +0300

    main-script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/olaf6642/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,50 @@
-// @todo: Темплейт карточки
+const mainContent = document.querySelector('.content');
+const placesList = mainContent.querySelector('.places__list');
+const addButton = mainContent.querySelector('.profile__add-button');
+const editButton = mainContent.querySelector('.profile__edit-button');
 
-// @todo: DOM узлы
+const popup = document.querySelector('.popup_type_new-card');
+const popClose = popup.querySelector('.popup__close');
+const saveButton = popup.querySelector('.popup__button');
 
-// @todo: Функция создания карточки
+function addCard(cardName, cardPicture) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardElement.querySelector('.card__image').src = cardPicture;
 
-// @todo: Вывести карточки на страницу
+	placesList.addEventListener('click', removeEvent);
+
+  placesList.append(cardElement);
+}
+
+addButton.addEventListener('click', function () { 							
+	popup.classList.add('popup_is-opened');
+});
+	
+popClose.addEventListener('click', function () {								
+	popup.classList.remove('popup_is-opened');
+})
+	
+saveButton.addEventListener('click', function () { 							
+	const placeName = popup.querySelector('.popup__input_type_card-name');
+  const placeLink = popup.querySelector('.popup__input_type_url');
+
+  addCard(placeName.value, placeLink.value);
+	popup.classList.remove('popup_is-opened');
+
+	placeName.value = '';
+	placeLink.value = '';
+});
+
+for (let i = 0; i < initialCards.length; i++) { 
+  addCard(initialCards[i].name, initialCards[i].link);
+}
+
+function removeEvent(e) {
+	if(e.target.classList.contains('card__delete-button')) {
+		placesList.removeChild(e.target.parentElement);
+		placesList.removeChild(placesList);
+	}
+}
